e309711bedbdbf08a5ca77abf29459696e3ff0cd
Oliwia <dsw54518@student.dsw.edu.pl>
2024-06-02 16:21:51 +0200

    task4

--- lab678/main.py
+++ lab678/main.py
@@ -1,5 +1,6 @@
 import sys
 import json
+import yaml
 
 def parsowanie():
     if len(sys.argv) != 3:
@@ -20,11 +21,27 @@ def json_zapis(data, file_path):
     with open (file_path, 'w') as file:
         json.dumb(data, file)
 
+def yaml(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            data = ymal(file)
+        return data
+    except yaml.YmalError as e:
+        print(f"Niepoprawny format: {e}")
+        sys.exit(1)
+
+
 if __name__ == "__main__":
     pathFile1, pathFile2 = parsowanie()
-    data = json(pathFile1)
-    print(data)
-    json_zapis(data, pathFile2)
+    if pathFile1.endswitch('.json'):
+        data = json(pathFile1)
+    elif pathFile1.endwitch('.yaml'):
+        data = ymal(pathFile1)
+    else:
+        print("Niepoprawny format.")
+        sys.exit(1)
 
+    json_zapis(data, pathFile2)
+    
 
 
