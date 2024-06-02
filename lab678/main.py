9fcceda3226462caaae7da7deca74510e0c29b40
Oliwia <dsw54518@student.dsw.edu.pl>
2024-06-02 16:04:11 +0200

    task2

--- lab678/main.py
+++ lab678/main.py
@@ -1,9 +1,26 @@
 import sys
+import json
+
 def parsowanie():
     if len(sys.argv) != 3:
         print("Użyj trzech argumentów")
         sys.exit(1)
     return sys.argv[1], sys.argv[2]
 
+def json(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            data = json(file)
+        return data
+    except json.JSONDecodeError as e:
+        print(f"Niepoprawny format: {e}")
+        sys.exit(1)
+
+
 if __name__ == "__main__":
     pathFile1, pathFile2 = parsowanie()
+    data = json(pathFile1)
+    print(data)
+
+
+
